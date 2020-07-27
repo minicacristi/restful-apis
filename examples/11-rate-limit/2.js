c65e2974a7d8d81c529a42503995a647f6a8efe1
Cristi Minica <cristi.minica@caphyon.com>
2020-07-27 15:55:26 +0300

    Update podium. Minor comment fix.

--- examples/11-rate-limit/2.js
+++ examples/11-rate-limit/2.js
@@ -4,7 +4,7 @@ const app = express();
 const erl = require('express-rate-limit');
 const erli = erl({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 9 // limit each IP to 100 requests per windowMs
+  max: 9 // limit each IP to 9 requests per windowMs
 });
 app.use(erli);
 
